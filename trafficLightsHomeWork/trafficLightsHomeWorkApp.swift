aae76138a56a2eb7d7cbd3eb25d9159c84eb93a6
mitya-toretto <dimahryasheff@yandex.ru>
2022-02-15 17:14:03 +0300

    Initial Commit

---
+++ trafficLightsHomeWork/trafficLightsHomeWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  trafficLightsHomeWorkApp.swift
+//  trafficLightsHomeWork
+//
+//  Created by Дмитрий on 15.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct trafficLightsHomeWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
